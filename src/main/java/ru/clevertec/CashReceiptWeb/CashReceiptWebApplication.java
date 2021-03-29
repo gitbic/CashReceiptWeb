25e66544196d53a16a5259ebaa95e31a50853720
Pavel Turski <pavelitelb@gmail.com>
2021-03-29 13:31:02 +0300

    Initialize Spring

---
+++ src/main/java/ru/clevertec/CashReceiptWeb/CashReceiptWebApplication.java
@@ -0,0 +1,13 @@
+package ru.clevertec.CashReceiptWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashReceiptWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashReceiptWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/clevertec/CashReceiptWeb/CashReceiptWebApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.clevertec.CashReceiptWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashReceiptWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
