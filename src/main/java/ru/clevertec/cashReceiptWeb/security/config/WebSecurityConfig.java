07d619bba98448c698ca0000d82e3a70343a7fb0
Pavel Turski <pavelitelb@gmail.com>
2021-04-04 00:08:26 +0300

    change a lot of login html pages

--- src/main/java/ru/clevertec/cashReceiptWeb/security/config/WebSecurityConfig.java
+++ src/main/java/ru/clevertec/cashReceiptWeb/security/config/WebSecurityConfig.java
@@ -43,10 +43,40 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/resources/**", "/registration").permitAll()
-                .anyRequest().authenticated().and()
-                .formLogin().loginPage("/login").permitAll().and()
-                .logout().permitAll();
+        http.csrf().disable();
+
+        // The pages does not require login
+        http.authorizeRequests().antMatchers("/", "/login", "/logout").permitAll();
+
+        // /userInfo page requires login as ROLE_USER or ROLE_ADMIN.
+        // If no login, it will redirect to /login page.
+        http.authorizeRequests().antMatchers("/userInfo").access("hasAnyRole('USER', 'ADMIN')");
+
+        // For ADMIN only.
+        http.authorizeRequests().antMatchers("/admin").access("hasRole('ADMIN')");
+
+        // When the user has logged in as XX.
+        // But access a page that requires role YY,
+        // AccessDeniedException will be thrown.
+        http.authorizeRequests().and().exceptionHandling().accessDeniedPage("/403");
+
+        // Config for Login Form
+        http.authorizeRequests().and().formLogin()//
+                // Submit URL of login page.
+                .loginProcessingUrl("/j_spring_security_check") // Submit URL
+                .loginPage("/login")//
+                .defaultSuccessUrl("/userAccountInfo")//
+                .failureUrl("/login?error=true")//
+                .usernameParameter("username")//
+                .passwordParameter("password")
+                // Config for Logout Page
+                .and().logout().logoutUrl("/logout").logoutSuccessUrl("/logoutSuccessful");
+
+
+//        http.authorizeRequests()
+//                .antMatchers("/resources/**", "/registration").permitAll()
+//                .anyRequest().authenticated().and()
+//                .formLogin().loginPage("/login").permitAll().and()
+//                .logout().permitAll();
     }
 }
--- src/main/java/ru/clevertec/cashReceiptWeb/security/controller/UserController.java
+++ src/main/java/ru/clevertec/cashReceiptWeb/security/controller/UserController.java
@@ -1,6 +1,7 @@
 package ru.clevertec.cashReceiptWeb.security.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -8,17 +9,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.util.WebUtils;
 import ru.clevertec.cashReceiptWeb.security.model.User;
 import ru.clevertec.cashReceiptWeb.security.service.SecurityService;
 import ru.clevertec.cashReceiptWeb.security.service.UserService;
 import ru.clevertec.cashReceiptWeb.security.validator.UserValidator;
 
+import java.security.Principal;
+
 @Controller
-@RequestMapping("/auth")
+//@RequestMapping("/auth")
 public class UserController {
     private final UserService userService;
     private final SecurityService securityService;
     private final UserValidator userValidator;
+
+    private final static String AUTH_DIR = "auth/";
 
     @Autowired
     public UserController(UserService userService, SecurityService securityService, UserValidator userValidator) {
@@ -27,41 +33,106 @@ public class UserController {
         this.userValidator = userValidator;
     }
 
-    @GetMapping("/registration")
-    public String registration(Model model) {
-        model.addAttribute("userForm", new User());
-        return "registration";
+    @GetMapping({ "/", "/welcome" })
+    public String welcomePage(Model model) {
+        model.addAttribute("title", "Welcome");
+        model.addAttribute("message", "This is welcome page!");
+        return "welcomePage";
     }
 
-    @PostMapping("/registration")
-    public String registration(@ModelAttribute("userForm") User userForm, BindingResult bindingResult) {
+    @GetMapping("/admin")
+    public String adminPage(Model model, Principal principal) {
 
-        userValidator.validate(userForm, bindingResult);
+        User loginedUser = (User) ((Authentication) principal).getPrincipal();
 
-        if (bindingResult.hasErrors()) {
-            return "registration";
+//        String userInfo = WebUtils.toString(loginedUser);
+        model.addAttribute("userInfo", loginedUser.toString());
+        return "adminPage";
+    }
+
+    @GetMapping("/login")
+    public String loginPage(Model model) {
+        return "loginPage";
+    }
+
+    @GetMapping("/logoutSuccessful")
+    public String logoutSuccessfulPage(Model model) {
+        model.addAttribute("title", "Logout");
+        return "logoutSuccessfulPage";
+    }
+
+    @GetMapping("/userInfo")
+    public String userInfo(Model model, Principal principal) {
+
+//        String userName = principal.getName();
+//
+//        System.out.println("User Name: " + userName);
+
+        User loginedUser = (User) ((Authentication) principal).getPrincipal();
+
+//        String userInfo = WebUtils.toString(loginedUser);
+        model.addAttribute("userInfo", loginedUser.toString());
+
+        return "userInfoPage";
+    }
+
+    @GetMapping("/403")
+    public String accessDenied(Model model, Principal principal) {
+
+        if (principal != null) {
+            User loginedUser = (User) ((Authentication) principal).getPrincipal();
+
+//            String userInfo = WebUtils.toString(loginedUser);
+
+            model.addAttribute("userInfo", loginedUser.toString());
+
+            String message = "Hi " + principal.getName() //
+                    + "<br> You do not have permission to access this page!";
+            model.addAttribute("message", message);
+
         }
 
-        userService.save(userForm);
-        securityService.autoLogin(userForm.getUsername(), userForm.getPassword());
-        return "redirect:/welcome";
+        return "403Page";
     }
 
-    @GetMapping("/loggin")
-    public String login(Model model, String error, String logout) {
-        if (error != null) {
-            model.addAttribute("error", "Username or password  is invalid");
-        }
 
-        if (logout != null) {
-            model.addAttribute("message", "You have been logged out successfully");
-        }
 
-        return "login";
-    }
 
-    @GetMapping({"/", "/welcome"})
-    public String welcome(Model model) {
-        return "welcome";
-    }
+//    @GetMapping("/registration")
+//    public String registration(Model model) {
+//        model.addAttribute("userForm", new User());
+//        return "registration";
+//    }
+//
+//    @PostMapping("/registration")
+//    public String registration(@ModelAttribute("userForm") User userForm, BindingResult bindingResult) {
+//
+//        userValidator.validate(userForm, bindingResult);
+//
+//        if (bindingResult.hasErrors()) {
+//            return "registration";
+//        }
+//
+//        userService.save(userForm);
+//        securityService.autoLogin(userForm.getUsername(), userForm.getPassword());
+//        return "redirect:/welcome";
+//    }
+//
+//    @GetMapping("/login")
+//    public String login(Model model, String error, String logout) {
+//        if (error != null) {
+//            model.addAttribute("error", "Username or password  is invalid");
+//        }
+//
+//        if (logout != null) {
+//            model.addAttribute("message", "You have been logged out successfully");
+//        }
+//
+//        return "login";
+//    }
+//
+//    @GetMapping({"/", "/welcome"})
+//    public String welcome(Model model) {
+//        return "welcome";
+//    }
 }
