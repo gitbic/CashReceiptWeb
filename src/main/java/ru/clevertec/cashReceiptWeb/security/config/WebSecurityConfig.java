eb196b7daa4ca44c13cee2574c395fa5934f7e66
Pavel Turski <pavelitelb@gmail.com>
2021-04-23 12:34:40 +0300

    enable security

--- src/main/java/ru/clevertec/cashReceiptWeb/security/config/WebSecurityConfig.java
+++ src/main/java/ru/clevertec/cashReceiptWeb/security/config/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package ru.clevertec.cashReceiptWeb.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -24,18 +25,17 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests().anyRequest().permitAll();
+//        http.csrf().disable().authorizeRequests().anyRequest().permitAll();
 
-//        http.csrf().disable()
-//                .authorizeRequests()
-//
-//                .antMatchers(HttpMethod.POST, "/users/**").permitAll()
-//                .antMatchers(HttpMethod.GET, "/products/**").permitAll()
-//                .antMatchers("/purchases/**").authenticated()
-//                .antMatchers("/order/**").authenticated()
-//                .anyRequest().hasRole("ADMIN")
-//                .and().httpBasic()
-//                .and().sessionManagement().disable();
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/users/**").permitAll()
+                .antMatchers(HttpMethod.GET, "/products/**").permitAll()
+                .antMatchers("/purchases/**").authenticated()
+                .antMatchers("/order/**").authenticated()
+                .anyRequest().hasRole("ADMIN")
+                .and().httpBasic()
+                .and().sessionManagement().disable();
 
         http.authorizeRequests().and().formLogin()
                 .loginProcessingUrl("/j_spring_security_check")
